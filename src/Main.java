b5e71f3f28172400610224b5bd37a9be45484506
Joshmay201 <josuesegura2005@gmail.com>
2023-05-16 18:29:01 -0600

    Hola Mundo desde Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Josue Segura");
+        String saludar = "Hola Mundo Desde Java";
+        System.out.println(saludar);
+        System.out.println("ejemplo " + saludar.toUpperCase());
     }
 }
